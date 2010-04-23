f7a989eca52236f14a9f8061cabb80ebed8e449e
Chang-Man Lee <chang-man.lee@windriver.com>
2010-04-23 12:51:36 +0900

    Gallery: Added routine to process Enter KeyEvent.
    Change-Id: I4e694a35e85e87f24b35cc4856dd9ea16e663778

--- src/com/android/camera/ImageGallery.java
+++ src/com/android/camera/ImageGallery.java
@@ -264,8 +264,25 @@ public class ImageGallery extends Activity implements
                             this, mDeletePhotoRunnable, getCurrentImage());
                 }
                 return true;
+            case KeyEvent.KEYCODE_ENTER:
+            case KeyEvent.KEYCODE_DPAD_CENTER:
+                if (event.getRepeatCount() == 0) {
+                    event.startTracking();
+                }
+                return true;
         }
         return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_ENTER
+             || keyCode == KeyEvent.KEYCODE_DPAD_CENTER)
+            && event.isTracking() && !event.isCanceled()) {
+            onImageClicked(mGvs.getCurrentSelection());
+            return true;
+        }
+        return super.onKeyUp(keyCode, event);
     }
 
     private boolean isPickIntent() {
